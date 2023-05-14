faff48df31a655b20369aa357f0fc352689793c4
Ch. Sai Kumar <96567025+95027@users.noreply.github.com>
2023-05-14 10:46:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Routings from "./components/Routings";
 import Footer from './components/footer/Footer';
 import Navbar from "./components/header/Navbar";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
       <Routings/>
       <Footer/>
     </div>
